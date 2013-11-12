1aa699ddc52e473dbd5b022eb747fea628acfad1
Krasimir Atanasov <stkgstudio@gmail.com>
2013-11-12 17:18:59 +0200

    Client can view pickings

--- web/src/main/java/madesy/web/servlets/client/ViewPickingsServlet.java
+++ web/src/main/java/madesy/web/servlets/client/ViewPickingsServlet.java
@@ -8,18 +8,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import madesy.model.User;
+import madesy.model.pickings.PickingService;
 import madesy.web.utils.RequestManager;
 
 @WebServlet("/view-pickings")
 public class ViewPickingsServlet extends HttpServlet {
 	private static final long serialVersionUID = -7682281396626429728L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doGet(final HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		new RequestManager(request, response) {
-			
+
 			@Override
 			public String request() {
+				User loggedUser = (User) request.getSession().getAttribute("user");
+				PickingService pickingService = (PickingService) request
+						.getServletContext().getAttribute("pickingService");
+
+				request.setAttribute("pickings", pickingService
+						.getPickingsByClientId(loggedUser.getId()));
 				
 				return "client/view_pickings.jsp";
 			}
--- web/src/main/webapp/client/view_pickings.jsp
+++ web/src/main/webapp/client/view_pickings.jsp
@@ -4,5 +4,16 @@
 <t:genericpage>
 <jsp:body>
 	<h1>View pickings</h1>
+	<ul>
+	<c:forEach var="picking" items="${pickings}">
+    	<li>
+    	<c:out value="Picking id: ${picking.id}, " />
+    	<c:out value="Size: ${picking.size.width}:${picking.size.height}:${picking.size.depth}"/>
+    	<c:out value="Receiver: ${picking.receiver.name}, Receiver address: ${picking.receiver.address}"/>
+    	<c:out value="Status: ${picking.pickingStates}"/>
+    	<button>Track</button>
+    	</li>
+	</c:forEach>
+	</ul>
 </jsp:body>
 </t:genericpage>
